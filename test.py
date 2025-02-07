db309ea1e5dc0ccd707a58a37a3f1f528cd97eff
Alen Zguric <zguric.alen@hotmail.com>
2025-02-07 13:56:30 +0100

    `Removed unused import and added new functionality to test.py`

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
-from djitellopy import Tello
+# from djitellopy import Tello
 
-drone = Tello("192.168.137.145")
+# drone = Tello("192.168.137.145")
 
-drone.connect()
-print(drone.send_command_with_return("sn?"))
+# drone.connect()
+# print(drone.send_command_with_return("sn?"))
+import dron_pass
+
+print(dron_pass.getIps())
